a6b9d5ded11e73538de0160842212ec942f9ec61
vince_chung <hysltway@gmail.com>
2026-08-02 19:31:44 +0800

    fix: 刷新桌面应用图标资源

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -1,3 +1,13 @@
 fn main() {
+    for icon in [
+        "../icons/icon.ico",
+        "../icons/icon.png",
+        "../icons/32x32.png",
+        "../icons/128x128.png",
+        "../icons/128x128@2x.png",
+        "../icons/tray.png",
+    ] {
+        println!("cargo:rerun-if-changed={icon}");
+    }
     tauri_build::build()
 }
